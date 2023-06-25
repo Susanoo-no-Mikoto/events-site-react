8ac2808d3d2cf9e192c09e0ff746f93f967bc580
Susanoo-no-Mikoto <19300707@live.inueco.ru>
2023-06-25 18:56:25 +0500

    fix

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001/',
+  baseURL: 'https://json-server-events-site.onrender.com',
 });
 
 export default instance;
